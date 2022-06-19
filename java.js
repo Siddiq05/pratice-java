d360f7aaf9f985ba0480fe32a424f1da3d260d57
Siddiq05 <mustafasiddiq313@gmail.com>
2022-06-19 22:25:48 +0530

    Pratice

---
+++ java.js
@@ -0,0 +1,50 @@
+alert("hello world");
+
+var i=5;
+console.log(i);
+
+var x=2;
+var y=3;
+var sum=x+y;
+console.log(sum);
+
+var a=2;
+var b=3;
+var sum=x*y;
+console.log(sum);
+
+var a=2;
+var b=3;
+var sum=x-y;
+console.log(sum);
+
+var a=2;
+var b=3;
+var sum=x/y;
+console.log(sum);
+
+var o;
+console.log(o);
+
+var car = "BMW";
+console.log(typeof car, car);
+
+var age = 25;
+console.log(typeof age, age);
+
+var cake= true;
+console.log(typeof cake, cake);
+
+var hi = null;
+console.log(typeof hi, hi);
+
+var condition = undefined;
+console.log(typeof condition, condition);
+
+var vol = Symbol();
+console.log(typeof vol, vol);
+
+var car1 = "Audi A";
+var n=8;
+var sum=car1+n;
+console.log(typeof sum, sum);
